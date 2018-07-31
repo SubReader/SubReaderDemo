43971a6872b1b8c238dc0dca919f54ac1d25ce4f
Anders Bredgaard Thuesen <andersbthuesen@gmail.com>
2018-07-31 14:26:09 +0200

    Added fullscreen support

--- src/App.js
+++ src/App.js
@@ -91,135 +91,147 @@ class App extends Component {
     );
   }
 
-  renderVideoPlayer(video, video_id) {
-    const { videos, showAuthQR } = this.state;
+  renderAuthQR() {
+    const { authId } = this.state;
     return (
       <div
         style={{
-          margin: "0 auto",
-          display: "flex",
+          position: "absolute",
+          left: 0,
+          top: 0,
           width: "100%",
-          maxWidth: "960px"
+          height: "100%",
+          backgroundColor: "rgba(0, 0, 0, 0.75)",
+          color: "white",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center"
         }}
       >
         <div
-          className="player-container"
           style={{
-            width: "100%",
-            flex: 1
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center"
           }}
         >
           <div
             style={{
-              width: "100%",
-              position: "relative",
-              backgroundColor: "black"
+              backgroundColor: "white",
+              padding: "20px",
+              display: "inline-block"
             }}
           >
-            <VideoPlayer
-              autoplay={!this.state.showAuthQR}
-              controls={true}
-              fluid={true}
-              aspectRatio="16:9"
-              plugins={{
-                subreader: {
-                  stream$: this.stream$
-                }
-              }}
-              sources={[
-                {
-                  src: video.src,
-                  type: "video/mp4"
-                }
-              ]}
-              tracks={video.tracks}
-              poster={video.poster}
+            <QRCode
+              value={`subreader://authenticate?id=${authId}`}
+              level="M"
+              bgColor="transparent"
+              size={256}
             />
           </div>
-          <div style={{ padding: "0" }}>
-            <h1>{video.title}</h1>
-          </div>
+          <h2>Scan med SubReader appen</h2>
         </div>
+      </div>
+    );
+  }
+
+  renderVideoPlayer(video, video_id) {
+    const { videos, showAuthQR } = this.state;
+    const isFullscreen = window.location.hash === "#fullscreen";
+    return (
+      <div>
         <div
-          className="player-thumbnails"
           style={{
-            padding: "0px 10px",
-            width: "300px"
+            margin: "0 auto",
+            display: "flex",
+            width: "100%",
+            maxWidth: isFullscreen ? "none" : "960px"
           }}
         >
-          {Object.keys(videos)
-            .filter(id => id !== video_id)
-            .map(id => {
-              const video = videos[id];
-              return (
-                <Link
-                  key={id}
-                  to={`/${id}`}
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    margin: "20px 10px",
-                    cursor: "pointer",
-                    textDecoration: "none",
-                    color: "black"
-                  }}
-                  onClick={() => {
-                    console.log("Change nav");
-                  }}
-                >
-                  <img
-                    alt={video.title}
-                    src={video.poster}
-                    style={{ width: "120px", borderRadius: "2px", flex: 3 }}
-                  />
-                  <span style={{ marginLeft: "10px", flex: 2 }}>
-                    {video.title}
-                  </span>
-                </Link>
-              );
-            })}
-        </div>
-        {showAuthQR && (
           <div
+            className="player-container"
             style={{
-              position: "absolute",
-              left: 0,
-              top: 0,
               width: "100%",
-              height: "100%",
-              backgroundColor: "rgba(0, 0, 0, 0.75)",
-              color: "white",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center"
+              flex: 1
             }}
           >
             <div
               style={{
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center"
+                width: "100%",
+                position: "relative",
+                backgroundColor: "black"
               }}
             >
-              <div
-                style={{
-                  backgroundColor: "white",
-                  padding: "20px",
-                  display: "inline-block"
+              <VideoPlayer
+                autoplay={!this.state.showAuthQR}
+                controls={true}
+                fluid={true}
+                aspectRatio="16:9"
+                plugins={{
+                  subreader: {
+                    stream$: this.stream$
+                  }
                 }}
-              >
-                <QRCode
-                  value={`subreader://authenticate?id=${this.state.authId}`}
-                  level="M"
-                  bgColor="transparent"
-                  size={256}
-                />
-              </div>
-              <h2>Scan med SubReader appen</h2>
+                sources={[
+                  {
+                    src: video.src,
+                    type: "video/mp4"
+                  }
+                ]}
+                tracks={video.tracks}
+                poster={video.poster}
+              />
             </div>
+            {!isFullscreen && (
+              <div style={{ padding: "0" }}>
+                <h1>{video.title}</h1>
+              </div>
+            )}
           </div>
-        )}
+          {!isFullscreen && (
+            <div
+              className="player-thumbnails"
+              style={{
+                padding: "0px 10px",
+                width: "300px"
+              }}
+            >
+              {Object.keys(videos)
+                .filter(id => id !== video_id)
+                .map(id => {
+                  const video = videos[id];
+                  return (
+                    <Link
+                      key={id}
+                      to={`/${id}`}
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        margin: "20px 10px",
+                        cursor: "pointer",
+                        textDecoration: "none",
+                        color: "black"
+                      }}
+                      onClick={() => {
+                        console.log("Change nav");
+                      }}
+                    >
+                      <img
+                        alt={video.title}
+                        src={video.poster}
+                        style={{ width: "120px", borderRadius: "2px", flex: 3 }}
+                      />
+                      <span style={{ marginLeft: "10px", flex: 2 }}>
+                        {video.title}
+                      </span>
+                    </Link>
+                  );
+                })}
+            </div>
+          )}
+        </div>
+        {showAuthQR && this.renderAuthQR()}
       </div>
     );
   }
