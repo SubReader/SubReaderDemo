e211bffd9de24811a3feefe0da1578fcc13de2d7
Anders Bredgaard Thuesen <andersbthuesen@gmail.com>
2020-06-16 08:45:03 +0200

    Initial implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import * as SubReaderAPI from "subreader-api";
 import {
-  BrowserRouter as Router,
-  Route,
-  Redirect,
-  Switch,
-  Link
+	BrowserRouter as Router,
+	Route,
+	Redirect,
+	Switch,
+	Link,
 } from "react-router-dom";
 import videos from "./data/videos";
 import QRCode from "qrcode.react";
@@ -15,226 +15,212 @@ import VideoPlayer from "./VideoPlayer";
 videojs.registerPlugin("subreader", SubReaderVideoJSPlugin);
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      videos,
-      showAuthQR: false,
-      authId: ""
-    };
-  }
+	// getServiceToken() {
+	//   const serviceToken = localStorage.getItem("serviceToken");
+	//   const expiration = localStorage.getItem("serviceTokenExpiration");
+	//   if (serviceToken && expiration > Date.now()) {
+	//     return Promise.resolve(serviceToken);
+	//   }
+	//   return SubReaderAPI.getAuthToken()
+	//     .then(({ token: authToken, id: authId }) => {
+	//       this.setState({
+	//         showAuthQR: true,
+	//         authId
+	//       });
+	//       return SubReaderAPI.getServiceToken(authToken);
+	//     })
+	//     .then(({ service_token: serviceToken, expires_in: expiresIn }) => {
+	//       localStorage.setItem("serviceToken", serviceToken);
+	//       localStorage.setItem("serviceTokenExpiration", Date.now() + expiresIn);
+	//       this.setState({ showAuthQR: false });
+	//       return serviceToken;
+	//     });
+	// }
 
-  getServiceToken() {
-    const serviceToken = localStorage.getItem("serviceToken");
-    const expiration = localStorage.getItem("serviceTokenExpiration");
-    if (serviceToken && expiration > Date.now()) {
-      return Promise.resolve(serviceToken);
-    }
-    return SubReaderAPI.getAuthToken()
-      .then(({ token: authToken, id: authId }) => {
-        this.setState({
-          showAuthQR: true,
-          authId
-        });
-        return SubReaderAPI.getServiceToken(authToken);
-      })
-      .then(({ service_token: serviceToken, expires_in: expiresIn }) => {
-        localStorage.setItem("serviceToken", serviceToken);
-        localStorage.setItem("serviceTokenExpiration", Date.now() + expiresIn);
-        this.setState({ showAuthQR: false });
-        return serviceToken;
-      });
-  }
+	componentWillMount() {
+		// this.stream$ = this.getServiceToken()
+		//   .then(serviceToken => {
+		//     return SubReaderAPI.getStreamToken(serviceToken);
+		//   })
+		//   .then(({ token: streamToken, id: streamId }) => {
+		//     return new SubReaderAPI.Stream(streamToken, streamId);
+		//   });
+		// this.stream$.then((stream) => {
+		// 	stream.setInfo({
+		// 		title: video.title,
+		// 		cover: {
+		// 			uri: video.cover,
+		// 		},
+		// 		backdrop: {
+		// 			uri: video.backdrop,
+		// 		},
+		// 	});
+		// });
+	}
 
-  componentWillMount() {
-    this.stream$ = this.getServiceToken()
-      .then(serviceToken => {
-        return SubReaderAPI.getStreamToken(serviceToken);
-      })
-      .then(({ token: streamToken, id: streamId }) => {
-        return new SubReaderAPI.Stream(streamToken, streamId);
-      });
-  }
+	render() {
+		return (
+			<Router>
+				<div>
+					<Switch>
+						<Route
+							path="/:video_id"
+							render={({ match }) => {
+								const { video_id } = match.params;
+								const video = videos[video_id];
+								return this.renderVideoPlayer(video, video_id);
+							}}
+						/>
+						<Redirect
+							exact={true}
+							from="/"
+							to={`/${Object.keys(videos)[0] || "default"}`}
+						/>
+					</Switch>
+				</div>
+			</Router>
+		);
+	}
 
-  render() {
-    return (
-      <Router>
-        <div>
-          <Switch>
-            <Route
-              path="/:video_id"
-              render={({ match }) => {
-                const { video_id } = match.params;
-                const video = this.state.videos[video_id];
-                this.stream$.then(stream => {
-                  stream.setInfo({
-                    title: video.title,
-                    cover: {
-                      uri: video.cover
-                    },
-                    backdrop: {
-                      uri: video.backdrop
-                    }
-                  });
-                });
-                return this.renderVideoPlayer(video, video_id);
-              }}
-            />
-            <Redirect
-              exact={true}
-              from="/"
-              to={`/${Object.keys(this.state.videos)[0] || "default"}`}
-            />
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+	// renderAuthQR() {
+	// 	const { authId } = this.state;
+	// 	return (
+	// 		<div
+	// 			style={{
+	// 				position: "absolute",
+	// 				left: 0,
+	// 				top: 0,
+	// 				width: "100%",
+	// 				height: "100%",
+	// 				backgroundColor: "rgba(0, 0, 0, 0.75)",
+	// 				color: "white",
+	// 				display: "flex",
+	// 				alignItems: "center",
+	// 				justifyContent: "center",
+	// 			}}
+	// 		>
+	// 			<div
+	// 				style={{
+	// 					display: "flex",
+	// 					flexDirection: "column",
+	// 					justifyContent: "center",
+	// 					alignItems: "center",
+	// 				}}
+	// 			>
+	// 				<div
+	// 					style={{
+	// 						backgroundColor: "white",
+	// 						padding: "20px",
+	// 						display: "inline-block",
+	// 					}}
+	// 				>
+	// 					<QRCode
+	// 						value={`subreader://authenticate?id=${authId}`}
+	// 						level="M"
+	// 						bgColor="transparent"
+	// 						size={256}
+	// 					/>
+	// 				</div>
+	// 				<h2>Scan med SubReader appen</h2>
+	// 			</div>
+	// 		</div>
+	// 	);
+	// }
 
-  renderAuthQR() {
-    const { authId } = this.state;
-    return (
-      <div
-        style={{
-          position: "absolute",
-          left: 0,
-          top: 0,
-          width: "100%",
-          height: "100%",
-          backgroundColor: "rgba(0, 0, 0, 0.75)",
-          color: "white",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center"
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center"
-          }}
-        >
-          <div
-            style={{
-              backgroundColor: "white",
-              padding: "20px",
-              display: "inline-block"
-            }}
-          >
-            <QRCode
-              value={`subreader://authenticate?id=${authId}`}
-              level="M"
-              bgColor="transparent"
-              size={256}
-            />
-          </div>
-          <h2>Scan med SubReader appen</h2>
-        </div>
-      </div>
-    );
-  }
-
-  renderVideoPlayer(video, video_id) {
-    const { videos, showAuthQR } = this.state;
-    const isFullscreen = window.location.hash === "#fullscreen";
-    return (
-      <div>
-        <div
-          style={{
-            margin: "0 auto",
-            display: "flex",
-            width: "100%",
-            maxWidth: isFullscreen ? "none" : "960px"
-          }}
-        >
-          <div
-            className="player-container"
-            style={{
-              width: "100%",
-              flex: 1
-            }}
-          >
-            <div
-              style={{
-                width: "100%",
-                position: "relative",
-                backgroundColor: "black"
-              }}
-            >
-              <VideoPlayer
-                autoplay={!this.state.showAuthQR}
-                controls={true}
-                fluid={true}
-                aspectRatio="16:9"
-                plugins={{
-                  subreader: {
-                    stream$: this.stream$
-                  }
-                }}
-                sources={[
-                  {
-                    src: video.src,
-                    type: "video/mp4"
-                  }
-                ]}
-                tracks={video.tracks}
-                poster={video.poster}
-              />
-            </div>
-            {!isFullscreen && (
-              <div style={{ padding: "0" }}>
-                <h1>{video.title}</h1>
-              </div>
-            )}
-          </div>
-          {!isFullscreen && (
-            <div
-              className="player-thumbnails"
-              style={{
-                padding: "0px 10px",
-                width: "300px"
-              }}
-            >
-              {Object.keys(videos)
-                .filter(id => id !== video_id)
-                .map(id => {
-                  const video = videos[id];
-                  return (
-                    <Link
-                      key={id}
-                      to={`/${id}`}
-                      style={{
-                        display: "flex",
-                        alignItems: "center",
-                        margin: "20px 10px",
-                        cursor: "pointer",
-                        textDecoration: "none",
-                        color: "black"
-                      }}
-                      onClick={() => {
-                        console.log("Change nav");
-                      }}
-                    >
-                      <img
-                        alt={video.title}
-                        src={video.poster}
-                        style={{ width: "120px", borderRadius: "2px", flex: 3 }}
-                      />
-                      <span style={{ marginLeft: "10px", flex: 2 }}>
-                        {video.title}
-                      </span>
-                    </Link>
-                  );
-                })}
-            </div>
-          )}
-        </div>
-        {showAuthQR && this.renderAuthQR()}
-      </div>
-    );
-  }
+	renderVideoPlayer(video, video_id) {
+		const isFullscreen = window.location.hash === "#fullscreen";
+		return (
+			<div>
+				<div
+					style={{
+						margin: "0 auto",
+						display: "flex",
+						width: "100%",
+						maxWidth: isFullscreen ? "none" : "960px",
+					}}
+				>
+					<div
+						className="player-container"
+						style={{
+							width: "100%",
+							flex: 1,
+						}}
+					>
+						<div
+							style={{
+								width: "100%",
+								position: "relative",
+								backgroundColor: "black",
+							}}
+						>
+							<VideoPlayer
+								controls={true}
+								fluid={true}
+								aspectRatio="16:9"
+								plugins={{
+									subreader: {},
+								}}
+								sources={[
+									{
+										src: video.src,
+										type: "video/mp4",
+									},
+								]}
+								tracks={video.tracks}
+								poster={video.poster}
+							/>
+						</div>
+						{!isFullscreen && (
+							<div style={{ padding: "0" }}>
+								<h1>{video.title}</h1>
+							</div>
+						)}
+					</div>
+					{!isFullscreen && (
+						<div
+							className="player-thumbnails"
+							style={{
+								padding: "0px 10px",
+								width: "300px",
+							}}
+						>
+							{Object.keys(videos)
+								.filter((id) => id !== video_id)
+								.map((id) => {
+									const video = videos[id];
+									return (
+										<Link
+											key={id}
+											to={`/${id}`}
+											style={{
+												display: "flex",
+												alignItems: "center",
+												margin: "20px 10px",
+												cursor: "pointer",
+												textDecoration: "none",
+												color: "black",
+											}}
+											onClick={() => {
+												console.log("Change nav");
+											}}
+										>
+											<img
+												alt={video.title}
+												src={video.poster}
+												style={{ width: "120px", borderRadius: "2px", flex: 3 }}
+											/>
+											<span style={{ marginLeft: "10px", flex: 2 }}>
+												{video.title}
+											</span>
+										</Link>
+									);
+								})}
+						</div>
+					)}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/SubReaderVideoJSPlugin.js
+++ src/SubReaderVideoJSPlugin.js
@@ -3,6 +3,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import QRCode from "qrcode.react";
 
+import { StreamPlayer } from "@subreader/core";
+
 import { Observable } from "rxjs/Rx";
 import "./SubReaderVideoJSPlugin.css";
 
@@ -11,200 +13,324 @@ const MenuButton = videojs.getComponent("MenuButton");
 const Menu = videojs.getComponent("Menu");
 const Component = videojs.getComponent("Component");
 
+class LiveSubtitle {
+	delegate = null;
+
+	constructor(track) {
+		this.track = track;
+	}
+
+	load() {
+		const cues = [];
+		for (let i = 0; i < this.track.cues.length; i++) {
+			const trackCue = this.track.cues[i];
+			const cue = {
+				text: trackCue.text,
+				timeIn: trackCue.startTime * 1000,
+				timeOut: trackCue.endTime * 1000,
+			};
+			cues.push(cue);
+		}
+
+		this.delegate.onCues(cues);
+	}
+}
+
+class LiveStream {
+	delegate = null;
+
+	constructor(player) {
+		this.player = player;
+		this.player.on("pause", this.onPause);
+		this.player.on("ended", this.onPause);
+		this.player.on("playing", this.onPlay);
+		this.player.on("timeupdate", this.onSync);
+		this.player.on("seek", this.onTime);
+
+		this.player.textTracks().on("addtrack", this.onTracks);
+		this.onTracks();
+	}
+
+	liveTrackFor(track) {
+		switch (track.kind) {
+			case "subtitle": {
+				return {
+					...track,
+					track: new LiveSubtitle(track.subtitle),
+				};
+			}
+		}
+	}
+
+	load() {
+		this.onTracks();
+	}
+
+	onPause = () => {
+		this.delegate.onReceivePlaying(false);
+	};
+
+	onPlay = () => {
+		this.delegate.onReceivePlaying(true);
+	};
+
+	onSync = () => {
+		const time = this.player.currentTime() * 1000;
+		this.delegate.onReceiveSync(time);
+	};
+
+	onTime = () => {
+		const time = this.player.currentTime() * 1000;
+		this.delegate.onReceiveTime(time);
+	};
+
+	onTracks = () => {
+		const playerTracks = this.player.textTracks();
+		const tracks = [];
+		for (let i = 0; i < playerTracks.length; i++) {
+			const playerTrack = playerTracks[i];
+
+			switch (playerTrack.kind) {
+				case "subtitles": {
+					const subtitleTrack = {
+						subtitle: playerTrack,
+						id: playerTrack.language,
+						language: playerTrack.language,
+						kind: "subtitle",
+					};
+					tracks.push(subtitleTrack);
+				}
+			}
+		}
+		if (this.delegate) {
+			this.delegate.onReceiveTracks(tracks);
+		}
+	};
+}
+
 export default class SubReaderPlugin extends Plugin {
-  constructor(player, options) {
-    super(player, options);
+	constructor(player, options) {
+		super(player, options);
 
-    player.on("ready", () => {
-      player.controlBar.subreader = player.controlBar.addChild(
-        "subReaderMenuButton",
-        {
-          stream$: options.stream$
-        }
-      );
-      player.controlBar
-        .el()
-        .insertBefore(
-          player.controlBar.subreader.el(),
-          player.controlBar.el().firstChild.nextSibling
-        );
-    });
+		const streamPlayer = new StreamPlayer({
+			liveStream: new LiveStream(player),
+			delegate: {
+				didSelectTrack(track) {
+					console.log("select track", track);
+				},
+				didSelectVoice(voice) {
+					console.log("select voice", voice);
+				},
+				didSpeakCue(cue, index) {
+					console.log("speak cue", cue, index);
+				},
+				onCues(cues) {
+					console.log("cues", cues);
+				},
+				onInfo() {},
+				onPlaying(playing) {
+					console.log("playing", playing);
+				},
+				onTime(time) {
+					console.log("time", time);
+				},
+				onTracks(tracks) {
+					console.log("tracks", tracks);
+				},
+				onVoices(voices) {
+					console.log("voices", voices);
+				},
+			},
+		});
 
-    const $stream = Observable.from(options.stream$);
+		player.on("ready", () => {
+			player.controlBar.subreader = player.controlBar.addChild(
+				"subReaderMenuButton"
+			);
+			player.controlBar
+				.el()
+				.insertBefore(
+					player.controlBar.subreader.el(),
+					player.controlBar.el().firstChild.nextSibling
+				);
+		});
 
-    const $playing = Observable.create(observer => {
-      function onStart() {
-        observer.next(true);
-      }
-      function onStop() {
-        observer.next(false);
-      }
+		// const $stream = Observable.from(options.stream$);
 
-      player.on("pause", onStop);
-      player.on("ended", onStop);
-      player.on("playing", onStart);
+		// const $playing = Observable.create((observer) => {
+		// 	function onStart() {
+		// 		observer.next(true);
+		// 	}
+		// 	function onStop() {
+		// 		observer.next(false);
+		// 	}
 
-      return function dispose() {
-        player.off("pause", onStop);
-        player.off("ended", onStop);
-        player.off("playing", onStart);
-      };
-    });
+		// 	player.on("pause", onStop);
+		// 	player.on("ended", onStop);
+		// 	player.on("playing", onStart);
 
-    const $timeupdate = Observable.create(observer => {
-      function onTime() {
-        observer.next(Math.floor(player.currentTime() * 1000));
-      }
+		// 	return function dispose() {
+		// 		player.off("pause", onStop);
+		// 		player.off("ended", onStop);
+		// 		player.off("playing", onStart);
+		// 	};
+		// });
 
-      player.on("timeupdate", onTime);
+		// const $timeupdate = Observable.create((observer) => {
+		// 	function onTime() {
+		// 		observer.next(Math.floor(player.currentTime() * 1000));
+		// 	}
 
-      return function dispose() {
-        player.off("timeupdate", onTime);
-      };
-    });
+		// 	player.on("timeupdate", onTime);
 
-    const $timeseek = Observable.create(observer => {
-      function onTime() {
-        observer.next(Math.floor(player.currentTime() * 1000));
-      }
+		// 	return function dispose() {
+		// 		player.off("timeupdate", onTime);
+		// 	};
+		// });
 
-      player.on("seeking", onTime);
+		// const $timeseek = Observable.create((observer) => {
+		// 	function onTime() {
+		// 		observer.next(Math.floor(player.currentTime() * 1000));
+		// 	}
 
-      return function dispose() {
-        player.off("seeking", onTime);
-      };
-    });
+		// 	player.on("seeking", onTime);
 
-    const $time = $timeupdate
-      .throttle(() => Observable.interval(2000))
-      .merge($timeseek);
+		// 	return function dispose() {
+		// 		player.off("seeking", onTime);
+		// 	};
+		// });
 
-    const $state = Observable.combineLatest(
-      $playing,
-      $time,
-      (playing, time) => {
-        return {
-          playing,
-          time
-        };
-      }
-    );
+		// const $time = $timeupdate
+		// 	.throttle(() => Observable.interval(2000))
+		// 	.merge($timeseek);
 
-    const $subtitles = Observable.create(observer => {
-      const tracks = player.textTracks();
+		// const $state = Observable.combineLatest(
+		// 	$playing,
+		// 	$time,
+		// 	(playing, time) => {
+		// 		return {
+		// 			playing,
+		// 			time,
+		// 		};
+		// 	}
+		// );
 
-      function handleTracks() {
-        const subtitles$ = [];
+		// const $subtitles = Observable.create((observer) => {
+		// 	const tracks = player.textTracks();
 
-        for (let i = 0; i < tracks.length; i++) {
-          const track = tracks[i];
-          subtitles$.push(
-            new Promise(resolve => {
-              function handleLoadedData() {
-                const cues = [];
-                for (let j = 0; j < track.cues.length; j++) {
-                  const cue = track.cues[j];
-                  const div = document.createElement("div");
-                  div.innerHTML = cue.text;
-                  cues.push({
-                    timeIn: Math.floor(cue.startTime * 1000),
-                    timeOut: Math.floor(cue.endTime * 1000),
-                    text: div.innerText
-                  });
-                }
-                resolve({
-                  language: track.language,
-                  cues
-                });
-                track.removeEventListener("loadeddata", handleLoadedData);
-              }
-              track.oncuechange = handleLoadedData;
-              track.addEventListener("loadeddata", handleLoadedData);
-            })
-          );
-        }
+		// 	function handleTracks() {
+		// 		const subtitles$ = [];
 
-        Promise.all(subtitles$).then(subtitles => {
-          observer.next(subtitles);
-        });
-      }
+		// 		for (let i = 0; i < tracks.length; i++) {
+		// 			const track = tracks[i];
+		// 			subtitles$.push(
+		// 				new Promise((resolve) => {
+		// 					function handleLoadedData() {
+		// 						const cues = [];
+		// 						for (let j = 0; j < track.cues.length; j++) {
+		// 							const cue = track.cues[j];
+		// 							const div = document.createElement("div");
+		// 							div.innerHTML = cue.text;
+		// 							cues.push({
+		// 								timeIn: Math.floor(cue.startTime * 1000),
+		// 								timeOut: Math.floor(cue.endTime * 1000),
+		// 								text: div.innerText,
+		// 							});
+		// 						}
+		// 						resolve({
+		// 							language: track.language,
+		// 							cues,
+		// 						});
+		// 						track.removeEventListener("loadeddata", handleLoadedData);
+		// 					}
+		// 					track.oncuechange = handleLoadedData;
+		// 					track.addEventListener("loadeddata", handleLoadedData);
+		// 				})
+		// 			);
+		// 		}
 
-      tracks.addEventListener("addtrack", handleTracks);
+		// 		Promise.all(subtitles$).then((subtitles) => {
+		// 			observer.next(subtitles);
+		// 		});
+		// 	}
 
-      return function dispose() {
-        tracks.removeEventListener("addtrack", handleTracks);
-      };
-    });
+		// 	tracks.addEventListener("addtrack", handleTracks);
 
-    $stream.combineLatest($subtitles).subscribe(([stream, subtitles]) => {
-      console.log("Setting subtitles", subtitles);
-      stream.setSubtitles(subtitles);
-    });
+		// 	return function dispose() {
+		// 		tracks.removeEventListener("addtrack", handleTracks);
+		// 	};
+		// });
 
-    $stream.combineLatest($state).subscribe(([stream, state]) => {
-      stream.setState(state);
-    });
-  }
+		// $stream.combineLatest($subtitles).subscribe(([stream, subtitles]) => {
+		// 	console.log("Setting subtitles", subtitles);
+		// 	stream.setSubtitles(subtitles);
+		// });
+
+		// $stream.combineLatest($state).subscribe(([stream, state]) => {
+		// 	stream.setState(state);
+		// });
+	}
 }
 
 class QR extends React.Component {
-  state = {
-    isLoading: true,
-    streamId: null
-  };
-  componentWillMount() {
-    this.props.stream$.then(stream => {
-      this.setState({ streamId: stream.id, isLoading: false });
-    });
-  }
-  render() {
-    if (this.state.isLoading) return <div>Loading...</div>;
-    return <QRCode value={`subreader://${this.state.streamId}`} />;
-  }
+	state = {
+		isLoading: true,
+		streamId: null,
+	};
+	// componentWillMount() {
+	//   this.props.stream$.then(stream => {
+	//     this.setState({ streamId: stream.id, isLoading: false });
+	//   });
+	// }
+	render() {
+		if (this.state.isLoading) return <div>Loading...</div>;
+		return <QRCode value={`subreader://${this.state.streamId}`} />;
+	}
 }
 
 class SubReaderMenu extends Menu {
-  createEl() {
-    const el = document.createElement("div");
-    el.className = "vjs-subreader-qr-container";
+	createEl() {
+		const el = document.createElement("div");
+		el.className = "vjs-subreader-qr-container";
 
-    ReactDOM.render(<QR stream$={this.options_.stream$} />, el);
+		ReactDOM.render(<QR stream$={this.options_.stream$} />, el);
 
-    this.contentEl_ = el;
+		this.contentEl_ = el;
 
-    return el;
-  }
+		return el;
+	}
 }
 
 export class SubReaderMenuButton extends MenuButton {
-  constructor(player, options) {
-    super(player, options);
-    this.controlText("SubReader");
-  }
+	constructor(player, options) {
+		super(player, options);
+		this.controlText("SubReader");
+	}
 
-  update() {
-    const menu = this.createMenu();
+	update() {
+		const menu = this.createMenu();
 
-    if (this.menu) {
-      this.menu.dispose();
-      this.removeChild(this.menu);
-    }
+		if (this.menu) {
+			this.menu.dispose();
+			this.removeChild(this.menu);
+		}
 
-    this.menu = menu;
-    this.addChild(menu);
+		this.menu = menu;
+		this.addChild(menu);
 
-    this.buttonPressed_ = false;
-    this.menuButton_.el_.setAttribute("aria-expanded", "false");
-    this.show();
-  }
+		this.buttonPressed_ = false;
+		this.menuButton_.el_.setAttribute("aria-expanded", "false");
+		this.show();
+	}
 
-  createMenu() {
-    return new SubReaderMenu(this.player_, { stream$: this.options_.stream$ });
-  }
+	createMenu() {
+		return new SubReaderMenu(this.player_, { stream$: this.options_.stream$ });
+	}
 
-  buildCSSClass() {
-    return `vjs-subreader-button ${super.buildCSSClass()}`;
-  }
+	buildCSSClass() {
+		return `vjs-subreader-button ${super.buildCSSClass()}`;
+	}
 }
+
 Component.registerComponent("SubReaderMenu", SubReaderMenu);
 Component.registerComponent("SubReaderMenuButton", SubReaderMenuButton);
